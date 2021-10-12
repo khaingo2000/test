67ebdafa0b2efd0c000284c947d2abcf64e4a8e5
Ngo Van Khai <ngovankhai2000@gmail.com>
2021-10-12 22:39:29 +0700

    test nua ne

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hello<code>src/App.js</code> and save to reload.
+          hell bèo<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
